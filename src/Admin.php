fff894df2394a561261d3caf0b87a93c21dbe5fe
aididiao <zhangle0403@126.com>
2020-07-18 18:44:56 +0800

    first

---
+++ src/Admin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Le\Laravelip;
+
+class Admin
+{
+    public function printRunning()
+    {
+        echo 'running';
+    }
+}
+
+
---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Le\Laravelip;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        $this->app->singleton('admin', function () {
+            return new Admin;
+        });
+    }
+}
+
