f99900ab7f1f6456713b4324f48a1033f2a126f1
aididiao <zhangle0403@126.com>
2020-07-28 17:04:15 +0800

    whitelist

--- src/CheckIP.php
+++ src/CheckIP.php
@@ -18,27 +18,30 @@ class CheckIP
         $RequestIP=new RequestIP();
         $ip=$RequestIP->createFromGlobals()->getClientIp();
 //        $ip='124.133.163.112';
-        $list=DB::table('blacklist')->where('ip',$ip)->get();
+        $list = DB::table('blacklist')->where('ip', $ip)->get();
         //        var_dump(DB::getQueryLog());
 //        var_dump(DB::getQueryLog());
 
-        if(count($list)!=0){
+        if (count($list) != 0) {
             //---------throw a exception
             throw new Exception('package report:ip is in blacklist.');
         }
+
     }
     /*
      * self::addBlacklist($ip);
      */
     public static function addBlacklist($ip){
-//        DB::table('blacklist')->where('ip',$ip)->get();
-        DB::table('blacklist')->insert(
-            [
-                'ip' => $ip,
-                'created_at' => date('Y-m-d H:i:s',time()),
-                'updated_at' => date('Y-m-d H:i:s',time())
-            ]
-        );
+        $whitelist=DB::table('whitelist')->where('ip',$ip)->get();
+        if(count($whitelist)==0) {
+            DB::table('blacklist')->insert(
+                [
+                    'ip' => $ip,
+                    'created_at' => date('Y-m-d H:i:s', time()),
+                    'updated_at' => date('Y-m-d H:i:s', time())
+                ]
+            );
+        }
     }
     /*
      * 在/app/Exceptions/Handler.php中调用此方法
